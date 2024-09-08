2d3aaebe07d8a651d5e8fcb1be7b2d5ccf595aa4
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-09-08 11:10:08 +0600

    create access modifiers.java

---
+++ OOPS/AccessModifiers.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class AccessModifiers {
+    public static void main(String args[]){
+        BankAccount myAcc = new BankAccount();
+        myAcc.username =  "Hemel";
+        System.out.println(myAcc.username);  //possible to access as its public modifiers;
+        //myAcc.Password = "abc"; //not possible as its a private modifier
+        myAcc.setPassword("abcde");
+     
+        
+    }
+}
+
+class BankAccount{
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+}

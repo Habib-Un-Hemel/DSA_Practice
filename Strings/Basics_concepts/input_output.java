44febaae34b5b18e4b30d67dfd5505bb9cacade4
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-10-24 21:23:11 +0600

    length function in String

--- Strings/Basics_concepts/input_output.java
+++ Strings/Basics_concepts/input_output.java
@@ -3,7 +3,7 @@ public class input_output {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String name;
-        name = sc.next();
+        name = sc.nextLine();
         System.out.println(name);
     }
 }
---
+++ Strings/Basics_concepts/length.java
@@ -0,0 +1,7 @@
+
+public class length {
+    public static void main(String args[]){
+        String name = "Hemel";
+        System.out.println(name.length());
+    }
+}

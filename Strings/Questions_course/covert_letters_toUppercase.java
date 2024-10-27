af23bf847b7eb17a961a38578e1b6f88e4b46d02
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-10-27 15:31:41 +0600

    covert_letters_toUppercase problem

---
+++ Strings/Questions_course/covert_letters_toUppercase.java
@@ -0,0 +1,23 @@
+
+
+public class covert_letters_toUppercase {
+    public static void main(String args[]){
+        StringBuilder str = new StringBuilder("hi, i am hemel and loves to code"); 
+        StringBuilder sd = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sd.append(ch);
+        //system.out.println(sd);
+
+        for (int i =1 ;i<str.length() ;i++){
+            if(str.charAt(i)== ' ' && i< str.length()-1){
+                sd.append(str.charAt(i));
+                i++;
+                sd.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sd.append(str.charAt(i));
+            }
+        }System.out.println(sd);
+
+    } 
+}

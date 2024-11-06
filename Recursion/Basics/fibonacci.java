92aea09c0ceea54c7b39e7aea0cc59775b45aa09
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-11-06 09:19:25 +0600

    update fibonnaci

--- Recursion/Basics/fibonacci.java
+++ Recursion/Basics/fibonacci.java
@@ -1,9 +1,12 @@
 public class fibonacci {
     public static int fibo(int n){
-        if (n==0){
-            return 0 ;
-        }else if(n==1){
-            return 1;
+        // if (n==0){
+        //     return 0 ;
+        // }else if(n==1){
+        //     return 1;
+        // }
+        if (n==0 || n==1){
+            return n;
         }
 
         int fibn_1 = fibo(n-1);

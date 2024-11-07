ace837d8204c8a0a61dd5fadfe0c7e82a845a58c
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-11-07 08:51:12 +0600

    optimized power problem

--- Recursion/Basics/power.java
+++ Recursion/Basics/power.java
@@ -10,10 +10,44 @@ public class power {
         // int xnm1 = powerTo(x, n-1);
         // int xn = x * xnm1;
         // return xn;
+        //O(n)
 
         return x * powerTo(x, n-1);
     }
+
+    public static int optimized_powerTo(int x,int n){
+
+    if (n==0){
+        return 1;
+    }
+
+    // int halfPowerSQ =  optimized_powerTo(x,n/2) * optimized_powerTo(x,n/2);
+    // if (n %2 ==0){
+    //     return halfPower;
+    // }else{
+    //     return x * halfPower;
+    // }
+    // }
+    // //O (n)
+
+    int halfPower =  optimized_powerTo(x,n/2);
+    int halfPowerSq = halfPower * halfPower;
+    if (n %2 ==0){
+        return halfPowerSq;
+    }else{
+        return x * halfPowerSq;
+    }
+    }
+    //O (logn) akber recursion call 
+
+
+
+
+
     public static void main(String args[]){
         System.out.println(powerTo(2, 5));
+        System.out.println(optimized_powerTo(2, 10));
+
     }
 }
+

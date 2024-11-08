1af7095117611979276fccb341cdf740a6312f7a
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-11-08 15:33:00 +0600

    titling problem

---
+++ Recursion/recursion2/tiling_problem.java
@@ -0,0 +1,22 @@
+
+public class tiling_problem {
+
+    public static int tilingProblem(int n){
+        if (n==0 || n==1){
+            return 1;
+        }
+        //vertical choice
+        int way1= tilingProblem(n-1);
+
+        //horizontal choice 
+        int way2= tilingProblem(n-2);
+        int all_ways = way1 + way2;
+
+        return all_ways;
+
+
+    }
+    public static void main(String args[]){
+        System.out.print(tilingProblem(10));
+    }
+}

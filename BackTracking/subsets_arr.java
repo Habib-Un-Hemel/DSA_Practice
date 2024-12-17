62df4fe1a63427c6354e336bb4009948db2dd489
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-12-17 23:57:28 +0600

    create subarray

---
+++ BackTracking/subsets_arr.java
@@ -0,0 +1,15 @@
+package BackTracking;
+
+public class subsets_arr {
+
+    public static void findSubset(int arr[], int i , int num){
+
+    }
+
+    
+    public static void main( String args[]){
+        int arr[]= {1,2,3};
+        findSubset(arr, 0, )
+
+    }
+}

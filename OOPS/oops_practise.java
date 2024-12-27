e76155e69a04957ca67c7e7cdb66d1d667c27845
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-12-27 13:05:16 +0600

    oops recap

---
+++ OOPS/oops_practise.java
@@ -0,0 +1,13 @@
+
+public class oops_practise {
+     public static void main(String[] args) {
+           Student s1 = new Student(); 
+    }
+}
+
+
+class Student{
+    String name;
+    int roll;
+
+}

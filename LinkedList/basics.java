ec4441c5ba5d5facbb38522ab4f707aaee0ee0a0
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-12-28 14:30:50 +0600

    update

--- LinkedList/basics.java
+++ LinkedList/basics.java
@@ -12,6 +12,17 @@ public class basics {
         Node c = new Node(9); 
         Node d = new Node(8); 
         Node e = new Node(5); 
-        System.out.println(a.data);
+
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.println(a.next);
+        System.out.println(b);
+        System.out.println();
+        System.out.println(b.next);
+        System.out.println(c);
     }
 }
+ 

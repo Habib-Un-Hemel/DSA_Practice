1f7d3080b09b1f4b816ab1176085d308084e0c1f
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-12-31 13:32:23 +0600

    update

--- Stacks/moveInSameOder.java
+++ Stacks/moveInSameOder.java
@@ -1,5 +1,24 @@
 package Stacks;
-import.java.Stack;
+import java.util.Stack;
 public class moveInSameOder {
-    
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5); 
+        Stack<Integer> gt = new Stack<>();
+        while(st.size()>0){
+            gt.push(st.pop());
+        }
+        Stack<Integer> rt = new Stack<>();
+        while(gt.size()>0){
+            rt.push(gt.pop());
+        }
+        System.out.println(st);
+        System.out.println(gt);
+        System.out.println(rt);
+    }
 }
+//O(2n) => O(n)

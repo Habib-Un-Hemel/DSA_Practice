20e1b628dc0708f4cb76b34e1c35e037233dd3b1
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2024-12-31 12:33:27 +0600

    update basic stacks

--- Stacks/basics.java
+++ Stacks/basics.java
@@ -1,5 +1,26 @@
 package Stacks;
-
+import java.util.Stack;
+ 
 public class basics {
-    
+  public static void main(String[] args) {
+    Stack<Integer> st = new Stack<>();
+    st.push(1);
+    st.push(11);
+    st.push(21);
+    st.push(41);
+    st.push(3);
+    st.push(9);
+    System.out.println(st.peek());
+    System.out.println(st);
+    System.out.println(st.pop());
+    System.out.println(st);
+    System.out.println(st.size());
+    System.out.println(st.isEmpty());
+
+    //print the first element
+    while( st.size()>1){
+        st.pop();
+    }System.out.println(st.peek());
+
+  }  
 }

110547f6476e040e2ec8480659ffc560a9395237
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-02-10 10:55:58 +0600

    Hashset by apna college

--- hashing/All_Hashing/a_hashset_apna.java
+++ hashing/All_Hashing/a_hashset_apna.java
@@ -1,7 +1,46 @@
-package hashing.All_Hashing;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class a_hashset_apna {
     public static void main(String[] args) {
-        System.out.println("hello wolrd");
+
+    //1. Initiate 
+    HashSet<Integer> set = new HashSet<>();
+    //example:creating relatetion ArrayList<Integer> list = new ArrayList<>();
+    
+    //2. insert (Add)
+    set.add(1);
+    set.add(2);
+    set.add(4);
+    set.add(1);
+    System.out.println(set);
+
+  //3. Search - contains
+
+   if(set.contains(2)){
+     System.out.println("2 is inside");
+   }
+    if(!set.contains(3)){
+      System.out.println("3 nai");
     }
+
+  //4.delete -> remove
+    set.remove(2);
+    System.out.println(set);
+
+  //5.size
+    System.out.println("Size of the HashSet is: "+set.size());
+
+
+  //6.iterator
+  Iterator it = set.iterator();
+  //hasNext ; next
+  while(it.hasNext()){
+      System.out.println(it.next());
+    }
+    
+  }
 }
+
+
+

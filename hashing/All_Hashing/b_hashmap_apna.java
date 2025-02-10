39d4c6ac06b5499e563979ac36ce8318b5f04095
Habibun Nabi Hemel <habibun33hemel@gmail.com>
2025-02-10 15:33:27 +0600

    update

--- hashing/All_Hashing/b_hashmap_apna.java
+++ hashing/All_Hashing/b_hashmap_apna.java
@@ -1,5 +1,12 @@
 import java.util.*;
 public class b_hashmap_apna {
+    //put()
+    //get()
+    //containsKey()
+    //remove()
+    //size()
+    //keySet()
+
     public static void main(String[] args) {
         System.out.println("Hello World");
 
---
+++ hashing/All_Hashing/c_implementation_hashmap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class c_implementation_hashmap {
+ public static void main(String[] args) {
+    System.out.println("Hello world");
+
+
+
+    
+ }
+}
